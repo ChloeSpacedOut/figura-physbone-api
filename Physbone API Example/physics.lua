b1f34989e67663d30ac1be431ca73d573250d7e9
ChloeSpacedOut <chloespacedout@gmail.com>
2023-10-22 22:55:49 +1000

    Complete Rework to everything :)
    I love changing everything in one commit!
    Moved the gimbal lock to the side.
    Removed the rotation correction, reusing elements for a roll fix.
    Renamed the keyword to "physbone".
    Fixed the backlog outside of the example >~>
    Re-implemented lerping.

--- Physbone API Example/physics.lua
+++ Physbone API Example/physics.lua
@@ -39,7 +39,7 @@ function events.entity_init()
 	function findCustomParentTypes(path)
 		for k,v in pairs(path:getChildren()) do
 			local name = v:getName()
-			if string.find(name,'phys',0) and not (string.find(name,'PYC',0) or string.find(name,'RC',0))  then
+			if string.find(name,'physBone',0) and not (string.find(name,'PYC',0) or string.find(name,'RC',0))  then
 				physBone[name] = {
 					path = v,
 					pos = v:partToWorldMatrix():apply(),
@@ -53,6 +53,8 @@ function events.entity_init()
 						v:removeChild(j)
 					end
 				end
+				physBone[name].path:setRot(0,90,0)
+				physBone[name].path['PYC'..name]['RC'..name]:setRot(0,-90,0)
 			end
 			findCustomParentTypes(v)
 		end
@@ -80,7 +82,7 @@ function events.tick()
 
 		local direction = physBone[k].pos - pendulumBase
 		physBone[k].pos = pendulumBase + direction:normalized()
-		
+
 		-- Rotation Calcualtion
 		local relativeVec = (physBone[k].path:partToWorldMatrix()):invert():apply(pendulumBase + (physBone[k].pos - pendulumBase)):normalize()
 		
@@ -88,32 +90,18 @@ function events.tick()
 		yaw = math.deg(math.atan2(relativeVec.x,relativeVec.z))
 		pitch = math.deg(math.asin(-relativeVec.y))
 		physBone[k].rot = vec(pitch,0,yaw)
-		--- debug ----
+--[[ 		--- debug ----
 		for i = 0, 1, 1/16 do
 			local currentPos = pendulumBase + (physBone[k].pos - pendulumBase) * i
 			particles['dust 1 0 0 1']:pos(currentPos):setLifetime(1):scale(1/5):spawn()
---[[ 			local rotVec = physBone[k].pos + rotVec  * i/5
-			particles['dust 0 0 1 1']:pos(rotVec):setLifetime(1):scale(1/5):spawn() ]]
 		end
-		------------------
+		------------------ ]]
 	end
 end
 
 function events.render(delta)
-	local playerRot = player:getRot().y -- note that a system to choose between player rot and body yaw will be needed. Maybe a custom orientation dir option too
-	
 	for k,v in pairs(physBone) do
-		
-		local pendulumBase = getPos(k)
-		local direction = physBone[k].pos - pendulumBase
-		local rotVec = find_perpendicular_direction_vector(direction,playerRot)
-		local rotVec2 = (physBone[k].path['PYC'..k]:partToWorldMatrix()):invert():apply(pendulumBase + rotVec):normalize()
-		physBone[k].path['PYC'..k]['RC'..k]:setRot(0,-math.deg(math.atan(rotVec2.z,rotVec2.x)),0)
-
 		local path = physBone[k].path['PYC'..k]
-		--path:setRot(math.lerp(path:getRot(),physBone[k].rot,delta))
-		path:setRot(physBone[k].rot)
-		
-
+		path:setRot(math.lerp(path:getRot(),physBone[k].rot,delta))
 	end
 end
--- Physbone API/physics.lua
+++ Physbone API/physics.lua
@@ -6,83 +6,102 @@ local elapsedTime = 0 -- Milliseconds
 
 
 function updateTime()
-    currentTime = client:getSystemTime()
-    deltaTime = currentTime - previousTime
-    elapsedTime = elapsedTime + deltaTime
-    previousTime = currentTime
+	currentTime = client:getSystemTime()
+	deltaTime = currentTime - previousTime
+	elapsedTime = elapsedTime + deltaTime
+	previousTime = currentTime
+end
+
+function find_perpendicular_direction_vector(direction,playerRot)
+	-- Normalize the direction vector.
+	direction = direction:normalize()
+
+	local rotVector = vectors.angleToDir(0,playerRot-90)
+
+	-- Calculate the cross product of the direction vector and the z-axis.
+	perpendicular_direction_vector = direction:cross(vec(rotVector.x, 0, rotVector.z))
+
+	-- If the perpendicular direction vector is the zero vector, then return the negative z-axis.
+	if perpendicular_direction_vector == vec(0, 0, 0) then
+		return vec(-1, 0, 0)
+	else
+		return perpendicular_direction_vector
+	end
 end
 
 function getPos(ID)
-    return physBone[ID].path:partToWorldMatrix():apply()
+	return physBone[ID].path:partToWorldMatrix():apply()
 end
 
 function events.entity_init()
-    -- Pendulum object initialization
-    physBone = {}
-    function findCustomParentTypes(path)
-        for k,v in pairs(path:getChildren()) do
-            local name = v:getName()
-            if string.find(name,'phys',0) and not string.find(name,'physChild',0)  then
-                physBone[name] = {
-                    path = v,
-                    pos = v:partToWorldMatrix():apply(),
-                    lastPos = v:partToWorldMatrix():apply()
-                }
-                v:newPart('physChild'..name)
-                for i,j in pairs(v:getChildren()) do
-                    if j:getName() ~= 'physChild'..name then
-                        v['physChild'..name]:addChild(j)
-                        v:removeChild(j)
-                    end
-                end
-            end
-            findCustomParentTypes(v)
-        end
-    end
-    findCustomParentTypes(models)
-      
-    
-  --[[   pendulum = {
-        position = getPosition(),
-        previousPosition = getPosition()
-    } ]]
+	-- Pendulum object initialization
+	physBone = {}
+	function findCustomParentTypes(path)
+		for k,v in pairs(path:getChildren()) do
+			local name = v:getName()
+			if string.find(name,'physBone',0) and not (string.find(name,'PYC',0) or string.find(name,'RC',0))  then
+				physBone[name] = {
+					path = v,
+					pos = v:partToWorldMatrix():apply(),
+					lastPos = v:partToWorldMatrix():apply()
+				}
+				v:newPart('PYC'..name)
+				v['PYC'..name]:newPart('RC'..name)
+				for i,j in pairs(v:getChildren()) do
+					if j:getName() ~= 'PYC'..name then
+						v['PYC'..name]['RC'..name]:addChild(j)
+						v:removeChild(j)
+					end
+				end
+				physBone[name].path:setRot(0,90,0)
+				physBone[name].path['PYC'..name]['RC'..name]:setRot(0,-90,0)
+			end
+			findCustomParentTypes(v)
+		end
+	end
+	findCustomParentTypes(models)
 end
 
 function events.tick()
-    updateTime()
-    local deltaTimeInSeconds = deltaTime / 1000 -- Delta Time in seconds
+	updateTime()
+	local deltaTimeInSeconds = deltaTime / 1000 -- Delta Time in seconds
 
-    
-    for k,v in pairs(physBone) do
+	for k,v in pairs(physBone) do
 
-        -- Pendulum logic
-        local pendulumBase = getPos(k)
-        local velocity = physBone[k].pos - physBone[k].lastPos
+		-- Pendulum logic
+		local pendulumBase = getPos(k)
+		local velocity = physBone[k].pos - physBone[k].lastPos
 
-        -- Air Resistance
-        local airResistanceFactor = 0.1 -- Adjust this value to control the strength of air resistance
-        local airResistance = velocity * (-airResistanceFactor)
-        velocity = velocity + airResistance
+		-- Air Resistance
+		local airResistanceFactor = 0.1 -- Adjust this value to control the strength of air resistance
+		local airResistance = velocity * (-airResistanceFactor)
+		velocity = velocity + airResistance
 
-        physBone[k].lastPos = physBone[k].pos:copy()
-        physBone[k].pos = physBone[k].pos + velocity + vec(0, -10* (deltaTimeInSeconds^2), 0)
+		physBone[k].lastPos = physBone[k].pos:copy()
+		physBone[k].pos = physBone[k].pos + velocity + vec(0, -10* (deltaTimeInSeconds^2), 0)
 
-        local direction = physBone[k].pos - pendulumBase
-        physBone[k].pos = pendulumBase + direction:normalized()
+		local direction = physBone[k].pos - pendulumBase
+		physBone[k].pos = pendulumBase + direction:normalized()
 
-        -- Rotation Calcualtion
-        local relativeVec = (physBone[k].path:partToWorldMatrix()):invert():apply(pendulumBase + (physBone[k].pos - pendulumBase)):normalize()
-        
-        relativeVec = vectors.rotateAroundAxis(90,relativeVec,vec(-1,0,0))
-        yaw = math.deg(math.atan2(relativeVec.x,relativeVec.z))
-        pitch = math.deg(math.asin(-relativeVec.y))
-        physBone[k].rot = math.lerp(physBone[k].path['physChild'..k]:getRot(),vec(pitch,0,yaw),0.5)
-    end
+		-- Rotation Calcualtion
+		local relativeVec = (physBone[k].path:partToWorldMatrix()):invert():apply(pendulumBase + (physBone[k].pos - pendulumBase)):normalize()
+		
+		relativeVec = vectors.rotateAroundAxis(90,relativeVec,vec(-1,0,0))
+		yaw = math.deg(math.atan2(relativeVec.x,relativeVec.z))
+		pitch = math.deg(math.asin(-relativeVec.y))
+		physBone[k].rot = vec(pitch,0,yaw)
+--[[ 		--- debug ----
+		for i = 0, 1, 1/16 do
+			local currentPos = pendulumBase + (physBone[k].pos - pendulumBase) * i
+			particles['dust 1 0 0 1']:pos(currentPos):setLifetime(1):scale(1/5):spawn()
+		end
+		------------------ ]]
+	end
 end
 
 function events.render(delta)
-    for k,v in pairs(physBone) do
-        local path = physBone[k].path['physChild'..k]
-        path:setRot(math.lerp(path:getRot(),physBone[k].rot,delta))
-    end
+	for k,v in pairs(physBone) do
+		local path = physBone[k].path['PYC'..k]
+		path:setRot(math.lerp(path:getRot(),physBone[k].rot,delta))
+	end
 end
