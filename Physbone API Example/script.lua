2ca5e96c5e39eb08bc7a3f358d8f1c982ae069b6
ChloeSpacedOut <chloespacedout@gmail.com>
2024-07-07 21:38:53 +1000

    Added Back Custom Spring Example

--- Physbone API Example/script.lua
+++ Physbone API Example/script.lua
@@ -1,5 +1,5 @@
 require('physBoneAPI')
---[[ function events.entity_init()
-  physBone.physBoneBouncy:setSpringForce(500)
+function events.entity_init()
+  physBone.physBoneBouncy:setSpringForce(50)
   physBone.physBoneBouncy:setEquilibrium(vec(0,0))
-end ]]
+end
