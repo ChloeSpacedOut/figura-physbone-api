c58bc02a7b702aa73e11fdd12b32d66139f90a7c
ChloeSpacedOut <chloespacedout@gmail.com>
2024-07-07 21:31:45 +1000

    An Update :3
    - moved physbones to render for significantly more accurate physics
    - replaced physbone transformations with matrix transformations, instead of adding groups to the avatar
    - changed spring equilibrium to be relative to the model part, and use a rotation vec2
    - added setUpsideDown() and getUpsideDown() so to account for physbones with the pivot at the bottom of the bone
    - added physBoob preset
    - added physEar preset

--- Physbone API Example/script.lua
+++ Physbone API Example/script.lua
@@ -1,8 +1,5 @@
 require('physBoneAPI')
-function events.entity_init()
-  physBone.physBoneBouncy:setSpringForce(0.5)
-end
-
-function events.tick()
-  physBone.physBoneBouncy:setEquilibrium(-player:getLookDir())
-end
+--[[ function events.entity_init()
+  physBone.physBoneBouncy:setSpringForce(500)
+  physBone.physBoneBouncy:setEquilibrium(vec(0,0))
+end ]]
